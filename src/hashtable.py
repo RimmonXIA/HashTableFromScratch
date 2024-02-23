5f8d4ef090be1c517a9ba5fc54c33f183bb1ddeb
xiaruilong <ruilongxia@126.com>
2024-02-23 09:53:41 +0800

    feat: find a value by key

--- src/hashtable.py
+++ src/hashtable.py
@@ -11,3 +11,26 @@ class HashTable:
     def __setitem__(self, key, value):
         idx = hash(key) % len(self)
         self.values[idx] = value
+    
+    def __getitem__(self, key):
+        idx = hash(key) % len(self)
+        value = self.values[idx]
+        if value is BLANK:
+            raise KeyError(key)
+        return value
+    
+    # for the 'in' operation
+    def __contains__(self, key):
+        try:
+            self[key]
+        except KeyError:
+            return False
+        else:
+            return True
+    
+    # mimic the 'dict.get()' behavior
+    def get(self, key, default=None):
+        try:
+            return self[key]
+        except KeyError:
+            return default
