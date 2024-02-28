a3e97ec76caca103404bd5620fbb9a27e14be348
xiaruilong <ruilongxia@126.com>
2024-02-28 09:45:36 +0800

    feat: report the hashtable length

--- src/hashtable.py
+++ src/hashtable.py
@@ -1,3 +1,5 @@
+#%% 
+
 from typing import NamedTuple, Any
 
 class Pair(NamedTuple):
@@ -6,32 +8,34 @@ class Pair(NamedTuple):
 
 class HashTable:
     def __init__(self, capacity) -> None:
-        self._pairs = capacity * [None]
-
+        if capacity > 1:
+            self._slots = capacity * [None]
+        else:
+            raise ValueError(capacity)
     # for `len(obj)`
     def __len__(self):
-        return len(self._pairs)
+        return len(self.pairs)
 
     # for `item[key] = value`
     def __setitem__(self, key, value):
-        self._pairs[self._index(key)] = Pair(key, value)
+        self._slots[self._index(key)] = Pair(key, value)
 
     # for "item[key]"
     def __getitem__(self, key):
-        pair = self._pairs[self._index(key)]
+        pair = self._slots[self._index(key)]
         if pair is None:
             raise KeyError(key)
         return pair.value
 
     def __delitem__(self, key):
         if key in self:
-            self._pairs[self._index(key)] = None
+            self._slots[self._index(key)] = None
         else:
             raise KeyError(key)
 
     # where all the `hash` magic happens
     def _index(self, key):
-        return hash(key) % len(self)
+        return hash(key) % self.capacity
 
     # for the 'in' operation
     def __contains__(self, key):
@@ -54,7 +58,7 @@ class HashTable:
     # pair shall be unique because of the the unique of the key
     @property
     def pairs(self):
-        return {pair for pair in self._pairs if pair}
+        return {pair for pair in self._slots if pair}
 
     # for `item.values`, may have & should return duplicate values
     @property
@@ -65,3 +69,14 @@ class HashTable:
     @property
     def keys(self):
         return {pair.key for pair in self.pairs}
+
+    @property
+    def capacity(self):
+        return len(self._slots)
+
+# #%%
+# hash_table = HashTable(capacity=100)
+
+# # %%
+# hash_table["missing_key"]
+# # %%
--- test/test_hashtable.py
+++ test/test_hashtable.py
@@ -13,11 +13,14 @@ def hash_table():
 def test_should_create_hashtable():
     assert HashTable(capacity=100) is not None
 
-def test_should_report_capacity():
-    assert len(HashTable(capacity=100)) == 100
-    
-def test_should_create_empty_value_slots():
-    assert [None] * 3 == HashTable(capacity=3)._pairs
+def test_should_report_capacity_of_empty_hash_table():
+    assert HashTable(capacity=100).capacity == 100
+
+def test_should_report_capacity(hash_table):
+    assert hash_table.capacity == 100
+
+def test_should_create_empty_pair_slots():
+    assert HashTable(capacity=3)._slots == [None] * 3
     
 def test_should_insert_key_value_pairs():
     # Given
@@ -32,7 +35,7 @@ def test_should_insert_key_value_pairs():
     assert ("hola", "hello") in hash_table.pairs
     assert (98.6, 37) in hash_table.pairs
     assert (False, True) in hash_table.pairs
-    assert len(hash_table) == 100
+    assert len(hash_table) == 3
 
 @pytest.mark.skip
 def test_should_not_shrink_when_removing_elements():
@@ -78,13 +81,13 @@ def test_should_get_value_with_default(hash_table):
 def test_should_delete_key_value_pair(hash_table):
     assert ("hola", "hello") in hash_table.pairs
     assert "hola" in hash_table
-    assert len(hash_table) == 100
+    assert len(hash_table) == 3
         
     del hash_table["hola"]
     
     assert ("hola", "hello") not in hash_table.pairs
     assert "hola" not in hash_table
-    assert len(hash_table) == 100
+    assert len(hash_table) == 2
 
 def test_should_raise_key_error_when_delete(hash_table):
     with pytest.raises(KeyError) as exception_info:
@@ -99,7 +102,7 @@ def test_should_update_value(hash_table):
     assert hash_table["hola"] == "hallo"
     assert hash_table[98.6] == 37
     assert hash_table[False] == True
-    assert len(hash_table) == 100
+    assert len(hash_table) == 3
     
 def test_should_return_pairs(hash_table):
     assert hash_table.pairs == {
@@ -149,3 +152,17 @@ def test_should_convert_to_dict(hash_table):
     assert set(dictionary.items()) == hash_table.pairs
     assert set(dictionary.keys()) == hash_table.keys
     assert list(dictionary.values()) == unordered(hash_table.values)
+    
+def test_should_report_length_of_empty_hash_table():
+    assert len(HashTable(capacity=100)) == 0
+
+def test_should_report_length(hash_table):
+    assert len(hash_table) == 3
+
+def test_should_not_create_hashtable_with_zero_capacity():
+    with pytest.raises(ValueError):
+        HashTable(capacity=0)
+
+def test_should_not_create_hashtable_with_negative_capacity():
+    with pytest.raises(ValueError):
+        HashTable(capacity=-100)
